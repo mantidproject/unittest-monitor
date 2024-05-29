94aad68c01bfb8db4480e58e43630a50f5188971
Jonathan Haigh <jonathan.haigh@stfc.ac.uk>
2024-05-29 16:40:01 +0100

    add runs to results in a safer way

--- ctest_log_parser.py
+++ ctest_log_parser.py
@@ -12,7 +12,7 @@ class CtestOutputParser:
     def __init__(self, path_to_log_file: str):
         self.log_path: str = path_to_log_file
         self.test_results: Dict[str, TestResult] = {}
-        self.result_regex = re.compile(r"(.*)Test *\#\d+: ([a-zA-Z0-9_\.]+) [\.\*]+( {3}Passed|Failed) +(\d+\.\d+) sec")
+        self.result_regex = re.compile(r".*Test *\#\d+: ([a-zA-Z0-9_\.]+) [\.\*]+( {3}Passed|Failed) +(\d+\.\d+) sec")
 
     def parse(self) -> Dict[str, TestResult]:
         logger.info(f"Parsing {self.log_path}")
@@ -20,13 +20,13 @@ class CtestOutputParser:
             results_line_matches = [self.result_regex.match(line) for line in log_reader.readlines() if self.result_regex.match(line)]
 
         for match in results_line_matches:
-            num, name, result, time_taken = match.groups()
+            name, result, time_taken = match.groups()
             run = TestRun(name, (result.strip() == "Passed"), float(time_taken))
-            if num.strip():
+            if name in self.test_results:
+                self.test_results[name].add_run(run)
+            else:
                 # first run
                 self.test_results[name] = TestResult(run)
-            else:
-                self.test_results[name].add_run(run)
 
         logging.info(f"Found {len(self.test_results)} test results")
         return self.test_results
